4a408330c4e836121001b77892858941abed4240
etherbop <etherbop2@gmail.com>
2022-10-10 14:12:35 -0400

    imported html/template to wiki.go

--- wiki.go
+++ wiki.go
@@ -78,14 +78,10 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
 		"<form action=\"/save/%s\" method=\"POST\">"+
-		"<textarea name=\"body\">%s</txtarea><br>"+
+		"<textarea name=\"body\">%s</textarea><br>"+
 		"<input type=\"submit\" value=\"Save\">"+
 		"</form>",
 		p.Title, p.Title, p.Body)
-}
-
-func saveHandler(w http) {
-
 }
 
 func main() {
